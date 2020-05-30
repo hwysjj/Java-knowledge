b6c8050a83c20e1d071fa02f67dbb28e7d57f4ad
whua <william_cumt@hotmail.com>
2020-05-30 17:54:46 +0800

    ioc with maven

---
+++ spring_study/src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import com.hwy.bean.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("ioc.xml");
+        Person person = context.getBean("person", Person.class);
+        System.out.println(person);
+    }
+}
