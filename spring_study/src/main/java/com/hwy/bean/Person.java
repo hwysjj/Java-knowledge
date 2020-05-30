5766c363a53b3e295f5c44c43e539bb0437ccb4c
whua <william_cumt@hotmail.com>
2020-05-31 02:09:21 +0800

    ioc set multiple type properties

--- spring_study/src/main/java/com/hwy/bean/Person.java
+++ spring_study/src/main/java/com/hwy/bean/Person.java
@@ -1,13 +1,93 @@
 package com.hwy.bean;
 
+import java.util.*;
+
 public class Person {
     private int id;
     private String name;
-    private int age;
+    private Integer age;
     private String gender;
+    private Address address;
+    private List<Address> lists;
+    private Set<String> sets;
+    private Map<String,Object> maps;
+    private String[] hobbies;
+
+    public String[] getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String[] hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Address> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<Address> lists) {
+        this.lists = lists;
+    }
+
+    public Set<String> getSets() {
+        return sets;
+    }
+
+    public void setSets(Set<String> sets) {
+        this.sets = sets;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    private Properties properties;
 
     public Person() {
         System.out.println("构建Person成功！");
+    }
+
+    public Person(int id, String name, Integer age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Person(int id, String name, String gender) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public Person(int id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
     }
 
     public int getId() {
@@ -49,6 +129,12 @@ public class Person {
                 ", name='" + name + '\'' +
                 ", age=" + age +
                 ", gender='" + gender + '\'' +
+                ", address=" + address +
+                ", lists=" + lists +
+                ", sets=" + sets +
+                ", maps=" + maps +
+                ", hobbies=" + Arrays.toString(hobbies) +
+                ", properties=" + properties +
                 '}';
     }
 }
